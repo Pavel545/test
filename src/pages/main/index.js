9ab04048b92adbd9a0486dfbe0fbdf137a6de464
Pavel545 <110118389+Pavel545@users.noreply.github.com>
2022-11-11 10:09:32 +0400

    master

---
+++ src/pages/main/index.js
@@ -0,0 +1,12 @@
+import { Footer } from "../../componenst/footer";
+import { Header } from "../../componenst/hedear";
+
+export function Main() {
+    return(
+        <div>
+            <Header/>
+
+            <Footer/>
+        </div>
+    )
+}
